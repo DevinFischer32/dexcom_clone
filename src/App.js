10998a9a4c5cc81d7f4b1e035ec01b9f70070192
Devin Fischer <devinfischer32@gmail.com>
2023-01-17 21:53:53 -0800

    Added Front page from Dexcoms website

--- src/App.js
+++ src/App.js
@@ -2,9 +2,95 @@ import Header from "./Components/header";
 
 function App() {
   return (
-    <div className="bg-red-200">
+    <div>
       <Header />
-      <h1>Dexcom</h1>
+      <img src="" alt="" />
+      <section>
+        <h1>The power to manage your diabetes with confidence</h1>
+        <p>Make better diabetes decisions in the moment without fingersticks.*</p>
+        <p>*If your glucose alerts and readings from the G6 do not match symptoms or expectations, use a blood glucose meter to make diabetes treatment decisions.</p>
+        <button>Learn about Dexcom G6</button>
+      </section>
+      <section>
+        <h1>Dexcom G7 has received FDA clearance</h1>
+        <p>Compatible smart devices sold separately.†</p>
+        <button>Discover Dexcom G7</button>
+      </section>
+      <section>
+        <h1>the Dexcom CGM difference</h1>
+        <p>As the pioneer and leader in real-time continuous glucose monitoring (RT-CGM), Dexcom helps to simplify diabetes management. Our CGM systems provide exceptional accuracy1 and remarkable convenience, allowing you to live a healthier, more confident life.</p>
+        <div>
+          <h1>Nick Jonas, Dexcom user</h1>
+          <p>Paid spokesperson of Dexcom, Inc.</p>
+          <p>Compatible smart devices sold separately.†</p>
+        </div>
+      </section>
+      <section>
+        <h1>real-time glucose readings</h1>
+        <p>No fingersticks.* No scanning.</p>
+        <h1>easy to use</h1>
+        <p>Easy to apply, easy to set up, and easy to use2</p>
+        <h1>exceptional accuracy</h1>
+        <p>Dexcom offers clinically proven accuracy1</p>
+        <h1>proven results</h1>
+        <p>Lower A1C. More time in range3-4</p>
+        <h1>seamless connectivity</h1>
+        <p>Connects with insulin delivery systems‡</p>
+        <p>*If glucose alerts and readings from Dexcom G6 do not match symptoms or expectations, use a blood glucose meter to make diabetes treatment decisions.</p>
+        <div>
+          <button>Learn more about RT-CGM</button>
+          <button>See all Integrations</button>
+        </div>
+      </section>
+      <section>
+        <h1>Dexcom is the #1 most recommended CGM brand by people with diabetes5 and healthcare providers.6</h1>
+      </section>
+      <section>
+        <h1>Dexcom G6 is the most widely covered CGM system7</h1>
+        <p>This includes most commercial plans, Medicare,§ and more. Additionally, the average copay is $40, but many people pay $0 out of pocket||,8</p>
+        <button>Get a free benefits check</button>
+      </section>
+      <section>
+        <h1>“With Dexcom G6, it’s easy to look at the food I’m about to eat and understand where my glucose is heading. I’m a foodie again. Life is great.”</h1>
+        <p>David P., Dexcom user</p>
+        <div>
+          <button>See more stories</button>
+          <button>Become a Dexcom Warrior</button>
+        </div>
+      </section>
+      <section>
+        <h1>
+          get started with Dexcom CGM
+        </h1>
+        <button>Get started</button>
+      </section>
+      <section>
+        <div>
+          <h1>our experts are here to help.</h1>
+          <p>top of page icon</p>
+        </div>
+        <div>
+          <button>
+            <h1>customer support</h1>
+            <p>General Customer Support Monday-Friday 6:00am - 5:00pm PST</p>
+          </button>
+          <button>
+            <h1>product support</h1>
+            <p>product troubleshooting inquires 24 hours a day, 7 days a week</p>
+          </button>
+          <button>
+            <h1>live chat support</h1>
+            <p>Live Chat: 5:00 am - 5:00 pm PST</p>
+          </button>
+        </div>
+      </section>
+      <section>
+        <p>*If your glucose alerts and readings from the G6 do not match symptoms or expectations, use a blood glucose meter to make diabetes treatment decisions.</p>
+        <p>†For a list of compatible devices, visit dexcom.com/compatibility. ‡To learn more about insulin pump integrations and compatibility with Dexcom G6, visit https://www.dexcom.com/en-us/partnerships/integrations-and-compatibility. §Medicare covers Dexcom G6 for insulin-requiring patients who meet the Medicare coverage criteria. For the full list of Medicare coverage criteria, visit the Centers for Medicare and Medicaid services website. ||Refers to estimated out-of-pocket cost for eligible commercially insured patients from the Dexcom G6 sensor when claims are adjudicated as a pharmacy benefit, and includes benefits and offerings through available Dexcom programs, such as the voucher program. Actual cost may vary and is subject to individual insurance coverage.</p>
+        <p>1 Shah V LL, et al. Diabetes Technol Ther. 2018;20(6). 2 Dexcom, data on file, 2020. 3 Beck RW, et al. Ann Intern Med. 2018;168(7):526-7. 4 Welsh JB, et al. Diabetes Technol Ther. 2019;21(3):128-132. 5 dQ&A US Q1 2021 Diabetes Connections Patient Panel Report. 2021;69-72. 6 Seagrove HCP Survey Q1 2021. 2021;65. 7 Managed Markets Insights & Technology, LLC. MMIT Analytics, June 2022. 8 IQVIA February 2022.</p>
+        <p>BRIEF SAFETY STATEMENT Failure to use the Dexcom G6 Continuous Glucose Monitoring System (G6) and its components according to the instructions for use provided with your device and available at https://www.dexcom.com/en-us/safety-information and to properly consider all indications, contraindications, warnings, precautions, and cautions in those instructions for use may result in you missing a severe hypoglycemia (low blood glucose) or hyperglycemia (high blood glucose) occurrence and/or making a treatment decision that may result in injury. If your glucose alerts and readings from the G6 do not match symptoms or expectations or you’re taking over the recommended maximum dosage amount of 1000mg of acetaminophen every 6 hours, use a blood glucose meter to make diabetes treatment decisions. Seek medical advice and attention when appropriate, including for any medical emergency.</p>
+      </section>
+      <footer>footer</footer>
     </div>
   );
 }
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,11 +1,20 @@
 const header = () => {
   return (
-    <div>
-      <nav>
-        <a href="/">Home</a>
-        <a href=""></a>
-        <a href=""></a>
-      </nav>
+    <div className="flex justify-between">
+      <div className="flex">
+        <h1 className="bg-green-400 mr-2">Dexcom</h1>
+        <nav className="flex">
+          <p>Products</p>
+          <p>Learn</p>
+          <p>Community</p>
+          <p>Support</p>
+          <p className="bg-green-400">Get Started</p>
+        </nav>
+      </div>
+      <div>
+        <input type="text" placeholder="Search" />
+        <button>Icon</button>
+      </div>
     </div>
   );
 };
